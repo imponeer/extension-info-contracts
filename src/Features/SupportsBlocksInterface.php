85435ae2a6ce9213f14876dc2d1a4a65a559caeb
Raimondas Rimkevičius <github@mekdrop.name>
2021-02-16 02:12:37 +0200

    Added support for blocks descriptions

---
+++ src/Features/SupportsBlocksInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Imponeer\Contracts\ExtensionInfo\Features;
+
+use Imponeer\Contracts\ExtensionInfo\Elements\BlockInterface;
+
+/**
+ * If extension supports blocks must implement this interface
+ *
+ * @package Imponeer\Contracts\ExtensionInfo\Features
+ */
+interface SupportsBlocksInterface
+{
+
+    /**
+     * Gets blocks data
+     *
+     * @return BlockInterface[]
+     */
+    public function getBlocks(): iterable;
+
+}
